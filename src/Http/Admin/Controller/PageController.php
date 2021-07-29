f01d1041e85e796bf914d3516cc32b678e0cd41f
Mathieu <mathieugendron86@icloud.com>
2021-07-29 18:58:45 -0400

    feat: Dashboard Design + PageController For Administrator

--- src/Http/Admin/Controller/PageController.php
+++ src/Http/Admin/Controller/PageController.php
@@ -4,7 +4,15 @@
 namespace App\Http\Admin\Controller;
 
 
-class PageController
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends BaseController
 {
 
+  #[Route("/", name: 'home')]
+  public function home(): Response {
+
+    return $this->render('admin/pages/home.html.twig');
+  }
 }
