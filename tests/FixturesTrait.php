d2b5274b29d6b150c692d992e0e3b10f91185414
Mathieu <mathieugendron86@icloud.com>
2021-07-31 13:22:43 -0400

    fix: Disable Xdebug

---
+++ tests/FixturesTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests;
+
+
+use App\Helper\PathHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use Fidry\AliceDataFixtures\LoaderInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+
+/**
+ * @property EntityManagerInterface $em
+ */
+trait FixturesTrait
+{
+    protected KernelBrowser $client;
+    /**
+     * Charge une série de fixture en base de donnée et ajoute les entités à l'EntityManager.
+     *
+     * @param array<string> $fixtures
+     *
+     * @return array<string,object>
+     */
+    public function loadFixtures(array $fixtures): array
+    {
+        $fixturePath = $this->getFixturesPath();
+        $files = array_map(fn ($fixture) => PathHelper::join($fixturePath, $fixture.'.yaml'), $fixtures);
+        /** @var LoaderInterface $loader */
+        $loader = $this->client->getContainer()->get('fidry_alice_data_fixtures.loader.doctrine');
+
+        return $loader->load($files);
+    }
+
+    public function getFixturesPath()
+    {
+        return __DIR__.'/fixtures/';
+    }
+}
