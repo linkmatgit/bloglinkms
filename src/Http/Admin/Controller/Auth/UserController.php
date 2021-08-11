228590a58a2c02fd6cc56a44be71cacec7de75c9
mathieugendron <mathieugendron86@icloud.com>
2021-08-11 18:53:15 -0400

    feat: listing des user dans l'administration

--- src/Http/Admin/Controller/Auth/UserController.php
+++ src/Http/Admin/Controller/Auth/UserController.php
@@ -7,17 +7,43 @@ namespace App\Http\Admin\Controller\Auth;
 use App\Domain\Auth\Repository\UserRepository;
 use App\Domain\Auth\User;
 use App\Http\Admin\Controller\BaseController;
+use App\Http\Admin\Controller\CrudController;
+use App\Http\Helper\Paginator\PaginatorInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/users', name: 'user_')]
 class UserController extends BaseController
 {
 
-    public function __construct(private EntityManagerInterface $em)
+    public function __construct(
+        private EntityManagerInterface $em,
+        private RequestStack $requestStack,
+        private PaginatorInterface $paginator
+    ) {
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(QueryBuilder $query = null): Response
     {
+        $request = $this->requestStack->getCurrentRequest();
+        $query = $query ?: $this->em->getRepository(User::class)
+            ->createQueryBuilder('row')
+            ->orderBy('row.createdAt', 'DESC');
+        if ($request->get('q')) {
+            $query = $this->applySearch(trim($request->get('q')), $query);
+        }
+        $this->paginator->allowSort('row.id', 'row.title');
+        $rows = $this->paginator->paginate($query->getQuery());
+        return $this->render('admin/users/index.html.twig', [
+            'rows' => $rows,
+            'searchable' => true,
+        ]);
     }
 
     #[Route('/search', name: 'autocomplete')]
---
+++ src/Http/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controller;
+
+   use Symfony\Component\HttpFoundation\Response;
+   use Symfony\Component\Routing\Annotation\Route;
+
+final class UserController extends AbstractController
+{
+
+       #[Route('/user', name: 'user_show')]
+    public function home(): Response
+    {
+        return $this->render('index.html.twig', [
+            'menu' => 'home']);
+    }
+}
